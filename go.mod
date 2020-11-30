41032ea7c45c7b0a62c940d8d89147f15a13a921
João Silvestre <joao.p.silvestre@tecnico.ulisboa.pt>
2020-11-30 16:43:11 +0000

    Add get block with data processing

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module github.com/ipfs/go-ipfs-exchange-interface
+module github.com/JoaoSilvestre95/go-ipfs-exchange-interface
 
 require (
-	github.com/ipfs/go-block-format v0.0.2
+	github.com/JoaoSilvestre95/go-block-format v0.0.3
 	github.com/ipfs/go-cid v0.0.5
 )
--- interface.go
+++ interface.go
@@ -5,8 +5,8 @@ import (
 	"context"
 	"io"
 
-	blocks "github.com/ipfs/go-block-format"
-	cid "github.com/ipfs/go-cid"
+	blocks "github.com/JoaoSilvestre95/go-block-format"
+	"github.com/ipfs/go-cid"
 )
 
 // Interface defines the functionality of the IPFS block exchange protocol.
@@ -27,6 +27,7 @@ type Fetcher interface {
 	// GetBlock returns the block associated with a given key.
 	GetBlock(context.Context, cid.Cid) (blocks.Block, error)
 	GetBlocks(context.Context, []cid.Cid) (<-chan blocks.Block, error)
+	GetBlockWithDataProcessing(ctx context.Context, cid cid.Cid) (blocks.DataProcessedBlock, error)
 }
 
 // SessionExchange is an exchange.Interface which supports
